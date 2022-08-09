c8d577fde5002a2b0050afd38fbb360b7fb65b40
Bigorian <s.wittavat@gmail.com>
2022-08-10 00:57:26 +0700

    debugger2

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -17,7 +17,7 @@ const Certificates = () => {
           <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='code-aca1'><a href="https://sv1.picz.in.th/images/2022/07/26/XkxP9I.jpg" target="_blank"  className="text-decoration-none" rel="noreferrer">Learn HTML Course (Code Academy)</a></div>
+                <div className='code-aca1'><a href="https://sv1.picz.in.th/images/2022/07/26/XkxP9I.jpg" target="_blank"  className="text-decoration-none" rel="noopener noreferrer">Learn HTML Course (Code Academy)</a></div>
                 <small className='text-light'>Beginner</small>
               </div>
             </article>
@@ -25,7 +25,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='code-aca2'><a href="https://sv1.picz.in.th/images/2022/07/26/XkQSGf.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Learn Command Line Course (Code Academy)</a></div>
+                <div className='code-aca2'><a href="https://sv1.picz.in.th/images/2022/07/26/XkQSGf.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Learn Command Line Course (Code Academy)</a></div>
                 <small className='text-light'>Beginner</small>
               </div>
             </article>
@@ -33,7 +33,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='code-aca3'><a href="https://sv1.picz.in.th/images/2022/07/26/XkQcHl.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Learn Bootstrap Course (Code Academy)</a></div>
+                <div className='code-aca3'><a href="https://sv1.picz.in.th/images/2022/07/26/XkQcHl.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Learn Bootstrap Course (Code Academy)</a></div>
                 <small className='text-light'>Beginner</small>
               </div>
             </article>
@@ -41,7 +41,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='code-aca4'><a href="https://sv1.picz.in.th/images/2022/07/26/XkQqtV.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Learn CSS Course (Code Academy)</a></div>
+                <div className='code-aca4'><a href="https://sv1.picz.in.th/images/2022/07/26/XkQqtV.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Learn CSS Course (Code Academy)</a></div>
                 <small className='text-light'>Beginner</small>
               </div>
             </article>
@@ -49,7 +49,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='code-aca4'><a href="https://www.img.in.th/images/6f74a2fda2f3c5dcb3c133f6ac14274a.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Learn Express Course (Code Academy)</a></div>
+                <div className='code-aca4'><a href="https://www.img.in.th/images/6f74a2fda2f3c5dcb3c133f6ac14274a.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Learn Express Course (Code Academy)</a></div>
                 <small className='text-light'>Beginner</small>
               </div>
             </article>
@@ -57,7 +57,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='code-aca4'><a href="https://www.img.in.th/images/654785550d13b71c0b8779cdc534dcd4.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Learn React Course (Code Academy)</a></div>
+                <div className='code-aca4'><a href="https://www.img.in.th/images/654785550d13b71c0b8779cdc534dcd4.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Learn React Course (Code Academy)</a></div>
                 <small className='text-light'>Beginner</small>
               </div>
             </article>
@@ -65,7 +65,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='sut-xlane1'><a href="https://sv1.picz.in.th/images/2022/07/26/XkU7ME.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Data Analytics for Beginner (SUT X-lane)</a></div>
+                <div className='sut-xlane1'><a href="https://sv1.picz.in.th/images/2022/07/26/XkU7ME.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Data Analytics for Beginner (SUT X-lane)</a></div>
                 <small className='text-light'>Beginner</small>
               </div>
             </article>
@@ -73,7 +73,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='sut-xlane2'><a href="https://sv1.picz.in.th/images/2022/07/26/XkUEWR.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Cyber Security for Beginner (SUT X-lane)</a></div>
+                <div className='sut-xlane2'><a href="https://sv1.picz.in.th/images/2022/07/26/XkUEWR.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Cyber Security for Beginner (SUT X-lane)</a></div>
                 <small className='text-light'>Beginner</small>
               </div>
             </article>
@@ -81,7 +81,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='future'><a href="https://sv1.picz.in.th/images/2022/07/26/XkUed1.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Basic Programing HTML5/CSS/JS  (Future Skill)</a></div>
+                <div className='future'><a href="https://sv1.picz.in.th/images/2022/07/26/XkUed1.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Basic Programing HTML5/CSS/JS  (Future Skill)</a></div>
                 <small className='text-light'>Entry Level</small>
               </div>
             </article>
@@ -106,7 +106,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div className='card3-li'>
-                <div className='cswip'><a href="https://sv1.picz.in.th/images/2022/07/26/XkZvIb.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Visual Welding Inspector (CSWIP 3.0)</a></div>
+                <div className='cswip'><a href="https://sv1.picz.in.th/images/2022/07/26/XkZvIb.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Visual Welding Inspector (CSWIP 3.0)</a></div>
                 <small className='text-light'>Intermediate Level</small>
               </div>
             </article>
@@ -114,7 +114,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='council'><a href="https://sv1.picz.in.th/images/2022/07/26/XkZIVR.png" target="_blank" class="text-decoration-none" rel="noreferrer">Council of Mining Engineer (Metallurgical Field)</a></div>
+                <div className='council'><a href="https://sv1.picz.in.th/images/2022/07/26/XkZIVR.png" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Council of Mining Engineer (Metallurgical Field)</a></div>
                 <small className='text-light'>Association Level</small>
               </div>
             </article>
@@ -122,7 +122,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='steel'><a href="https://sv1.picz.in.th/images/2022/06/11/V7Hjqv.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Continuous Casting Process (World Steel Association) </a></div>
+                <div className='steel'><a href="https://sv1.picz.in.th/images/2022/06/11/V7Hjqv.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Continuous Casting Process (World Steel Association) </a></div>
                 <small className='text-light'>Intermediate Level</small>
               </div>
             </article>
@@ -130,7 +130,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='steel'><a href="https://www.img.in.th/images/0899a364d3d4003178a96256a5b8ae57.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Liquid Penetrant Testing      #Level 2 (CNI Inspection)  </a></div>
+                <div className='steel'><a href="https://www.img.in.th/images/0899a364d3d4003178a96256a5b8ae57.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Liquid Penetrant Testing      #Level 2 (CNI Inspection)  </a></div>
                 <small className='text-light'>Intermediate Level</small>
               </div>
             </article>
@@ -138,7 +138,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='steel'><a href="https://www.img.in.th/images/f121c24c7e70b3eeaf66adbd0a16504d.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">Mesurement System Analysis 4th Edition (Technology Promotion Assosiate Thailand-German) </a></div>
+                <div className='steel'><a href="https://www.img.in.th/images/f121c24c7e70b3eeaf66adbd0a16504d.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Mesurement System Analysis 4th Edition (Technology Promotion Assosiate Thailand-German) </a></div>
                 <small className='text-light'>Master Level</small>
               </div>
             </article>
@@ -146,7 +146,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='steel'><a href="https://www.img.in.th/images/43a57799915ef6b61bc7fe483d0e7eb0.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">IATF 16949 2016 "Requirement Interpretaion" (Entec Quality Management) </a></div>
+                <div className='steel'><a href="https://www.img.in.th/images/43a57799915ef6b61bc7fe483d0e7eb0.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">IATF 16949 2016 "Requirement Interpretaion" (Entec Quality Management) </a></div>
                 <small className='text-light'>Intermediate Level</small>
               </div>
             </article>
@@ -154,7 +154,7 @@ const Certificates = () => {
             <article className='certificates__details'>
               <GrCertificate className='certificates__details-icon' />
               <div>
-                <div className='steel'><a href="https://www.img.in.th/images/3cb49e36d2262555a4031a2ba6ef621c.jpg" target="_blank" class="text-decoration-none" rel="noreferrer">'IATF 16949' 2016 "Internal Audit" (Entec Quality Management) </a></div>
+                <div className='steel'><a href="https://www.img.in.th/images/3cb49e36d2262555a4031a2ba6ef621c.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">'IATF 16949' 2016 "Internal Audit" (Entec Quality Management) </a></div>
                 <small className='text-light'>Intermediate Level</small>
               </div>
             </article>
