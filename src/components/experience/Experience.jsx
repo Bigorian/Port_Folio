f0520695a6900d8621bbca4238985793f9036dee
Bigorian <s.wittavat@gmail.com>
2022-08-19 14:59:34 +0700

    Edit_myskill

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Experience.css';
-import { BsPatchCheckFill} from 'react-icons/bs';
+import { BsPatchCheckFill } from 'react-icons/bs';
 
 
 const Experience = () => {
   return (
     <section id='experience'>
       <h5>What Skills i Have</h5>
-      <h2>My Experience</h2>
+      <h2>My Skills</h2>
 
       <div className='container experience__container'>
         <div className='experience__frontend'>
@@ -38,13 +38,7 @@ const Experience = () => {
               </div>
             </article>
 
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Beginner</small>
-              </div>
-            </article>
+
           </div>
         </div>
 
@@ -77,7 +71,7 @@ const Experience = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>Python</h4>
+                <h4>SQL</h4>
                 <small className='text-light'>Beginner</small>
               </div>
             </article>
