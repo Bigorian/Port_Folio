1a4fa444823ace1e9c144b748eb199128a21f6f6
Bigorian <100525479+Bigorian@users.noreply.github.com>
2022-09-18 16:21:21 +0700

    add_alt_img_tag

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -36,7 +36,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-              <img src={Code_Academy} className='certificates__details-icon' />
+              <img src={Code_Academy} className='certificates__details-icon' alt='code_aca' />
               <div>
                 <div className='code-aca1'><a href="https://sv1.picz.in.th/images/2022/07/26/XkxP9I.jpg" target="_blank" className="text-decoration-none" rel="noopener noreferrer">Learn HTML Course (Code Academy)</a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -44,7 +44,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-              <img src={Code_Academy} className='certificates__details-icon' />
+            <img src={Code_Academy} className='certificates__details-icon' alt='code_aca' />
               <div>
                 <div className='code-aca2'><a href="https://sv1.picz.in.th/images/2022/07/26/XkQSGf.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Learn Command Line Course (Code Academy)</a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -52,7 +52,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-              <img src={Code_Academy} className='certificates__details-icon' />
+            <img src={Code_Academy} className='certificates__details-icon' alt='code_aca' />
               <div>
                 <div className='code-aca3'><a href="https://sv1.picz.in.th/images/2022/07/26/XkQcHl.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Learn Bootstrap Course (Code Academy)</a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -60,7 +60,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-              <img src={Code_Academy} className='certificates__details-icon' />
+            <img src={Code_Academy} className='certificates__details-icon' alt='code_aca' />
               <div>
                 <div className='code-aca4'><a href="https://sv1.picz.in.th/images/2022/07/26/XkQqtV.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Learn CSS Course (Code Academy)</a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -68,7 +68,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-              <img src={Code_Academy} className='certificates__details-icon' />
+            <img src={Code_Academy} className='certificates__details-icon' alt='code_aca' />
               <div>
                 <div className='code-aca4'><a href="https://www.img.in.th/images/6f74a2fda2f3c5dcb3c133f6ac14274a.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Learn Express Course (Code Academy)</a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -76,7 +76,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-              <img src={Code_Academy} className='certificates__details-icon' />
+            <img src={Code_Academy} className='certificates__details-icon' alt='code_aca' />
               <div>
                 <div className='code-aca4'><a href="https://www.img.in.th/images/654785550d13b71c0b8779cdc534dcd4.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Learn React Course (Code Academy)</a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -84,7 +84,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-              <img src={x_lane} className='certificates__details-icon' />
+              <img src={x_lane} className='certificates__details-icon' alt='x_lane'  />
               <div>
                 <div className='sut-xlane1'><a href="https://sv1.picz.in.th/images/2022/07/26/XkU7ME.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Data Analytics for Beginner (SUT X-lane)</a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -92,7 +92,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-              <img src={x_lane} className='certificates__details-icon' />
+            <img src={x_lane} className='certificates__details-icon' alt='x_lane' />
               <div>
                 <div className='sut-xlane2'><a href="https://sv1.picz.in.th/images/2022/07/26/XkUEWR.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Cyber Security for Beginner (SUT X-lane)</a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -100,7 +100,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-            <img src={future_skill} className='certificates__details-icon' />
+            <img src={future_skill} className='certificates__details-icon' alt='future' />
               <div>
                 <div className='future'><a href="https://sv1.picz.in.th/images/2022/07/26/XkUed1.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Basic Programing HTML5/CSS/JS  (Future Skill)</a></div>
                 <small className='text-light'>Entry Level</small>
@@ -108,7 +108,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-            <img src={monGo_DB} className='certificates__details-icon' />
+            <img src={monGo_DB} className='certificates__details-icon' alt='mongo' />
               <div>
                 <div className='mongoDB'><a href="https://www.img.in.th/images/55ce6eb454f6020dad6e2abfa2e76c48.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Mongo DB (Mongo DB University)</a></div>
                 <small className='text-light'>Beginner</small>
@@ -133,7 +133,7 @@ const Certificates = () => {
           <div className="certificates__content">
 
             <article className='certificates__details'>
-            <img src={cswip} className='certificates__details-icon' />
+            <img src={cswip} className='certificates__details-icon' alt='cswip' />
               <div className='card3-li'>
                 <div className='cswip'><a href="https://sv1.picz.in.th/images/2022/07/26/XkZvIb.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Visual Welding Inspector (CSWIP 3.0)</a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -141,7 +141,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-            <img src={council} className='certificates__details-icon' />
+            <img src={council} className='certificates__details-icon' alt='council' />
               <div>
                 <div className='council'><a href="https://sv1.picz.in.th/images/2022/07/26/XkZIVR.png" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Council of Mining Engineer (Metallurgical Field)</a></div>
                 <small className='text-light'>Association Level</small>
@@ -149,7 +149,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-            <img src={world_steel} className='certificates__details-icon' />
+            <img src={world_steel} className='certificates__details-icon' alt='world_steel' />
               <div>
                 <div className='steel'><a href="https://sv1.picz.in.th/images/2022/06/11/V7Hjqv.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Continuous Casting Process (World Steel Association) </a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -157,7 +157,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-            <img src={CNI} className='certificates__details-icon' />
+            <img src={CNI} className='certificates__details-icon' alt='cni' />
               <div>
                 <div className='steel'><a href="https://www.img.in.th/images/0899a364d3d4003178a96256a5b8ae57.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Liquid Penetrant Testing      #Level 2 (CNI Inspection)  </a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -165,7 +165,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-            <img src={thai_german} className='certificates__details-icon' />
+            <img src={thai_german} className='certificates__details-icon' alt='thai-german' />
               <div>
                 <div className='steel'><a href="https://www.img.in.th/images/f121c24c7e70b3eeaf66adbd0a16504d.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Mesurement System Analysis 4th Edition (Technology Promotion Assosiate Thailand-German) </a></div>
                 <small className='text-light'>Master Level</small>
@@ -173,7 +173,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-            <img src={mtec} className='certificates__details-icon' />
+            <img src={mtec} className='certificates__details-icon' alt='mtec' />
               <div>
                 <div className='steel'><a href="https://www.img.in.th/images/43a57799915ef6b61bc7fe483d0e7eb0.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">IATF 16949 2016 "Requirement Interpretaion" (Entec Quality Management) </a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -181,7 +181,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-            <img src={mtec} className='certificates__details-icon' />
+            <img src={mtec} className='certificates__details-icon' alt='mtec'/>
               <div>
                 <div className='steel'><a href="https://www.img.in.th/images/3cb49e36d2262555a4031a2ba6ef621c.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">IATF 16949' 2016 "Internal Audit" (Entec Quality Management) </a></div>
                 <small className='text-light'>Intermediate Level</small>
@@ -189,7 +189,7 @@ const Certificates = () => {
             </article>
 
             <article className='certificates__details'>
-            <img src={seda} className='certificates__details-icon' />
+            <img src={seda} className='certificates__details-icon' alt='seda'/>
               <div>
                 <div className='entrepreneur'><a href="https://www.img.in.th/images/bb563865b4f3d9cd1d8d491e4ed1fafa.jpg" target="_blank" class="text-decoration-none" rel="noopener noreferrer">Social Entrepreneur Ship and Start Up Camp (SEDA) </a></div>
                 <small className='text-light'>Intermediate Level</small>
